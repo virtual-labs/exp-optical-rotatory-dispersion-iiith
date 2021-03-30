e07098ea753c5dd111539dbaf588dab985d0a58b
sravanthimodepu <sravanthimodepu@gmail.com>
2021-03-30 23:22:09 +0530

    added sources for testing

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -89,24 +89,64 @@
 
 
      const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
+            question: "1. Molecular weight of a R enantiomer is __________ than that of S enantiomer. ", ///// Write the question inside double quotes
             answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                a: "Higher", ///// Write the option 1 inside double quotes
+                b: "Equal", ///// Write the option 2 inside double quotes
+		c: "Lower", ///// Write the option 1 inside double quotes
+                b: "can not say", ///// Write the option 2 inside double quotes
             },
-            correctAnswer: "a" ///// Write the correct option inside double quotes
+            correctAnswer: "b" ///// Write the correct option inside double quotes
         },
 
     {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
+      question: "2. In normal ORD spectra there are ___ maxima and __ minima.",  ///// Write the question inside double quotes
       answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
+        a: "1,2", ///// Write the option 1 inside double quotes
+        b: "1,1", ///// Write the option 2 inside double quotes
+        c: "0,0",
+	d: "0,1""
+      },
       correctAnswer: "c"                ///// Write the correct option inside double quotes
     },
 
 
+    {
+      question: "3. Octant rule determines __________ of Cotton effect in ketones. ",  ///// Write the question inside double quotes
+      answers: {
+        a: "magnitude", ///// Write the option 1 inside double quotes
+        b: "sign", ///// Write the option 2 inside double quotes
+        c: "probability",
+	d: "frequency"
+      },
+      correctAnswer: "b"                ///// Write the correct option inside double quotes
+    },
+
+
+    {
+      question: "4. Abnormal ORD is observed when _________ is present in a chiral environment. ",  ///// Write the question inside double quotes
+      answers: {
+        a: "chromophore", ///// Write the option 1 inside double quotes
+        b: "asymmetry center", ///// Write the option 2 inside double quotes
+        c: "chiral center",
+	d: "none of the above"
+      },
+      correctAnswer: "a"                ///// Write the correct option inside double quotes
+    },
+
+
+    {
+      question: "5. At the absorption peak an anomalous ORD curve _____________.",  ///// Write the question inside double quotes
+      answers: {
+        a: "reaches minimum", ///// Write the option 1 inside double quotes
+        b: "crosses the base line", ///// Write the option 2 inside double quotes
+        c: "reaches maximum",
+	d: "none of the above"
+      },
+      correctAnswer: "b"                ///// Write the correct option inside double quotes
+    },
+
+			  
 
      ];
 
