d19c10e0dcc13060e4c2dcf87bc993e788ba0413
vleadadmin <admin@vlabs.ac.in>
2025-04-17 15:04:44 +0000

    https://virtual-labs.github.io/virtual-labs/exp-optical-rotatory-dispersion-iiith click on the link to test your code.

---
+++ simulation/codes/js/overlay.js
@@ -0,0 +1,13 @@
+function on(x) {
+  s = "overlay" + x;
+  console.log(s);
+  document.getElementById(s).style.display = "block";
+}
+
+function off(x) {
+  s = "overlay" + x;
+  console.log(s);
+  document.getElementById(s).style.display = "none";
+  //document.getElementById("overlay1").style.display = "none";
+  //document.getElementById("overlay2").style.display = "none";
+}
